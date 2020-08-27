83b1b2f3e62fec8cdf2045bb3e16c49d31f4ef6d
miura0628 <yousanpu834@gmail.com>
2020-08-27 18:01:01 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+# get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
 end
